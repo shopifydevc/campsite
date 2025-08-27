d8e2095128f109c1e1994fb57f89987bb21d7ac9
Juli Sah <juli@truemark.com.np>
2025-08-27 18:30:52 +0545

    feat: deploy sync server

--- apps/sync-server/src/api.ts
+++ apps/sync-server/src/api.ts
@@ -3,7 +3,7 @@ import { Api } from '@campsite/types'
 let baseUrl = 'http://api.campsite.test:3001'
 
 if (process.env.NODE_ENV === 'production') {
-  baseUrl = 'http://campsite-api.internal:8080'
+  baseUrl = process.env.API_BASE_URL
 }
 
 export const api = new Api({
